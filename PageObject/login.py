8af512c686f138a5a0ab53ef089d6adc0bbb2f8a
Sayantan-nzminds <167063497+Sayantan-nzminds@users.noreply.github.com>
2024-05-24 19:20:32 +0530

    Update login.py

--- PageObject/login.py
+++ PageObject/login.py
@@ -12,6 +12,10 @@ class Login:
     email_input= "//input[@id='username']"
     password_input = "//input[@id='password']"
     account_signin_button = "//button[normalize-space()='Sign in']"
+    message_button = "//span[contains(@title,'Messaging')]"
+    meassage_search_Input = "//input[@placeholder='Type a name or multiple names']"
+    click_sayantan = "(//div[contains(@class,'msg-connections-typeahead__entity-description')])[1]"
+    message_send_button ="//button[normalize-space()='Send']"
     search_input= "//input[@placeholder='Search']"
 
 
@@ -26,10 +30,27 @@ class Login:
         self.driver.find_element(By.XPATH, self.email_input).send_keys(email)
         self.driver.find_element(By.XPATH, self.password_input).send_keys(password)
         self.driver.find_element(By.XPATH,self.account_signin_button).click()
+        time.sleep(3)
+
+    def Message(self,name):
+        self.driver.find_element(By.XPATH,self.message_button).click()
+        self.driver.find_element(By.XPATH,self.meassage_search_Input).send_keys(name)
 
 
-    def Search(self,search):
-        self.driver.find_element(By.XPATH,self.search_input).send_keys(search)
+
+        element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH,self.click_sayantan)))
+        element.click()
+        time.sleep(2)
+
+
+        element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH,self.message_send_button)))
+        element.click()
+
+        print("message is sent")
+
+
 
 
 
